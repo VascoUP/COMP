862b42165e2852c906844d211b89d9c3d16df082
Luis-bcruz <luis.labc95@gmail.com>
2017-05-25 19:08:51 +0100

    added cpp validate code generator

--- CompProj/src/codemaking/CProgram.java
+++ CompProj/src/codemaking/CProgram.java
@@ -58,8 +58,23 @@ public class CProgram implements ProgramMaker {
 		.append("using namespace std;\n\n");
 	}
 
+	public void writeCPPvalidate(StringBuilder text){
+		text.append("\nbool validate(string exp){\n"});
+		text.append("\tint curr_state = edges[0][0];\n");
+		text.append("\tfor(int i=0; i<exp.size();i++){\n");
+		text.append("\t\tint caracter = (int)exp[i];\n");
+		text.append("\t\tif (edges[curr_state][caracter] != -1)\n");
+		text.append("\t\t\tcurr_state = edges[curr_state][caracter];\n");
+		text.append("\t\telse\n");
+		text.append("\t\t\treturn false;\n");
+		text.append("\t{\n");
+		text.append("\treturn true;\n");
+		text.append("}");
+	}
+
 	private void writeCPPMain(StringBuilder text) {
 		text.append("\nint main() {\n");
+		text.append("");
 		text.append("\n\tstring str;\n");
 		text.append("\n\twhile(str != \"quit\") {\n");
 		text.append("\t\tcin >> str;\n");
