313bdcc67fcb77bc7133511258e9cd038087d340
VascoUP <vasco_mag_pereira@hotmail.com>
2017-06-01 14:46:57 +0100

    DFA minimization almost done

--- CompProj/src/automata/DFAMinimizer.java
+++ CompProj/src/automata/DFAMinimizer.java
@@ -1,7 +1,9 @@
 package automata;
 
 
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 public class DFAMinimizer {
     DFAMinimizer(AutomataTable dfa) {
@@ -27,5 +29,83 @@ public class DFAMinimizer {
         for(int i = 0; i < arr.length; i++)
             if( !arr[i] )
                 dfa.removeState(i);
+
+        // need to renumber automata id's
+    }
+
+    private void minimize(AutomataTable dfa) {
+        boolean[][] D = new boolean[dfa.getAutomataSize()][dfa.getAutomataSize()];
+        List<List<HashSet<Point>>> S = new ArrayList<>();
+
+        for (AutomataState state : dfa.getStateGrammar().keySet()) {
+            List<HashSet<Point>> innerList = new ArrayList<>();
+            for (AutomataState ignored : dfa.getStateGrammar().keySet()) {
+                Arrays.fill(D[state.getID()], false);
+                innerList.add(new HashSet<>());
+            }
+            S.add(innerList);
+        }
+
+        for (AutomataState state : dfa.getStateGrammar().keySet())
+            for (AutomataState state2 : dfa.getStateGrammar().keySet())
+                if (state.getAccept() != state2.getAccept())
+                    D[state.getID()][state2.getID()] = true;
+
+        for (AutomataState state : dfa.getStateGrammar().keySet())
+            for (AutomataState state2 : dfa.getStateGrammar().keySet()) {
+                if (D[state.getID()][state2.getID()])
+                    continue;
+
+                boolean distinguished = false;
+                HashMap<String, Set<AutomataState>> transitions = dfa.getStateGrammar().get(state);
+                for (Map.Entry<String, Set<AutomataState>> entry : transitions.entrySet()) {
+                    Set<AutomataState> Ms = entry.getValue();
+                    Set<AutomataState> Ns = dfa.getStateGrammar().get(state2).get(entry.getKey());
+                    if (Ms == null || Ms.size() != 1 || Ns == null || Ns.size() != 1)
+                        continue;
+
+                    int m = entry.getValue().iterator().next().getID();
+                    int n = Ns.iterator().next().getID();
+                    if (D[m][n] || D[n][m]) {
+                        dist(D, S, state.getID(), state2.getID());
+                        distinguished = true;
+                        break;
+                    }
+                }
+
+                if (!distinguished) {
+                    for (Map.Entry<String, Set<AutomataState>> entry : transitions.entrySet()) {
+                        Set<AutomataState> Ms = entry.getValue();
+                        Set<AutomataState> Ns = dfa.getStateGrammar().get(state2).get(entry.getKey());
+                        if (Ms == null || Ms.size() != 1 || Ns == null || Ns.size() != 1)
+                            continue;
+
+                        int m = entry.getValue().iterator().next().getID();
+                        int n = Ns.iterator().next().getID();
+
+                        if (m < n && !(state.getID() == m && state2.getID() == n))
+                            S.get(m).get(n).add(new Point(state.getID(), state2.getID()));
+                        else if (m > n && !(state.getID() == n && state2.getID() == m))
+                            S.get(n).get(m).add(new Point(state.getID(), state2.getID()));
+                    }
+                }
+            }
+
+        //mergeStates();
+    }
+
+    private void dist(boolean[][] D, List<List<HashSet<Point>>> S, int i, int j) {
+        _dist(D, S, new Point(i, j), new HashSet<>());
+    }
+
+    private void _dist(boolean[][] D, List<List<HashSet<Point>>> S, Point point, HashSet<Point> visited) {
+        if (visited.contains(point))
+            return;
+
+        int i = point.x, j = point.y;
+        D[i][j] = true;
+        visited.add(point);
+        for (Point pair : S.get(i).get(j))
+            _dist(D, S, pair, visited);
     }
 }
