08ebe45265daccffb83d3fa48c8b7d5025d3d7c4
VascoUP <vasco_mag_pereira@hotmail.com>
2017-06-04 09:48:18 +0100

    Vou ficar zangada

--- CompProj/src/codemaking/CProgram.java
+++ CompProj/src/codemaking/CProgram.java
@@ -1,4 +1,5 @@
 package codemaking;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -23,11 +24,7 @@ public class CProgram implements ProgramMaker {
 	@Override
 	public String code() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("#include<stdio.h>\n\n");
-		builder.append("int main(int argc, char* argv[]) {\n");
-		builder.append("	printf(\"Hello World!\\n\");\n");
-		builder.append("	return 0;\n");
-		builder.append("}\n");
+		writeCFile(builder);
 		return new String(builder);
 	}
 
@@ -49,36 +46,25 @@ public class CProgram implements ProgramMaker {
 		}  
 	}
 
-	private String writeCFile(StringBuilder text) {
-		StringBuilder builder = new StringBuilder();
-		
-		writeCIncludes(builder);
-		writeCValidate(builder);
-		writeCMain(builder);
-		
-		return new String(builder);
+	private void writeCFile(StringBuilder text) {
+		writeCIncludes(text);
+		writeCValidate(text);
+		writeCMain(text);
 	}
 
 	private void writeCIncludes(StringBuilder text) {
 		text.append("#include <stdio.h>\n");
 	}
 
-	public void writeCValidate(StringBuilder text){
-		HashMap<AutomataState, HashMap<String, Set<AutomataState>>> stateGrammar = table.getStateGrammar();
-
-		int hashSize = stateGrammar.size();
-		
-		text.append("\nint validate(char *exp, int edges[");
-		text.append(hashSize);
-		text.append("][256]){\n");
-		text.append("\tint currentState = edges[0][0];\n");
+	private void writeCValidate(StringBuilder text){
+		text.append("\nint validate(char *exp, int edges[][], int edgesSize){\n");
+		text.append("\tint currentState = 0;\n");
 		text.append("\tint i;\n");
-		text.append("\t\tint character = (int)exp[0];\n");
-		text.append("\t\tint size = sizeof(exp);\n");
+		text.append("\tint character = (int)exp[0];\n");
+		text.append("\tint size = sizeof(exp);\n");
 		text.append("\tfor(i = 0 ; i < size; i++){\n");
-		text.append("\t\tif(edges[currentState][character] != -1){\n");
-		text.append("\t\t\tcurrentState = edges[currentState][character];\n");
-		text.append("\t\t\tcharacter = (int)exp[i];\n");
+		text.append("\t\tif(edges[currentState][(int)exp[i]] != -1){\n");
+		text.append("\t\t\tcurrentState = edges[currentState][(int)exp[i]];\n");
 		text.append("\t\t}\n");
 		text.append("\t\telse\n");
 		text.append("\t\t\treturn 0;\n");
@@ -89,55 +75,56 @@ public class CProgram implements ProgramMaker {
 
 	private void writeCMain(StringBuilder text) {
 		text.append("\nint main(int argc, char* argv[]) {\n");
-		text.append("\n\tif(argc != 2){\n");
-		text.append("\n\t\tprintf(\"%s\", \"Wrong number of arguments\n\");\n");
+		text.append("\t\tif(argc != 2){\n");
+		text.append("\t\t\tprintf(\"%s\", \"Wrong number of arguments\n\");\n");
 		text.append("\t\t\treturn -1;\n");
-		text.append("\t}\n");
+		text.append("\t\t}\n");
 		
 		HashMap<AutomataState, HashMap<String, Set<AutomataState>>> stateGrammar = table.getStateGrammar();
 
 		int hashSize = stateGrammar.size();
 		
-		text.append("\tint edges[");
+		text.append("\t\tint edges[");
 		text.append(hashSize);
-		text.append("][256];\n");
-		
+		text.append("][256] = {");
+
 		int var = 0;
 		for (Entry<AutomataState, HashMap<String, Set<AutomataState>>> state : stateGrammar.entrySet()) {
 			AutomataState key = state.getKey();
 			HashMap<String, Set<AutomataState>> value = state.getValue();
-		
-			text.append("\tint map[256] = {");
+
+			if(var != 0)
+				text.append(",\n\t\t\t{");
+			else
+				text.append("\n\t\t\t{");
 			
-			if(key.equals("anyInput")){
+			if(value.get("anyInput") != null) {
 				text.append(key.getID());
-				text.append(" , ");
+				text.append(", ");
 			}
 			else{
 				int j;
-				for( j = 0; j < 256 ; j++){
-					for (Entry<String, Set<AutomataState>> entries : value.entrySet()) {
-						if(entries.getValue().isEmpty()){
-							text.append(" -1 ");
-							text.append(" , ");	
-						}
-						else{
-							for (AutomataState inputResult : entries.getValue()) {
-								text.append(inputResult.getID());
-								text.append(" , ");	
-							}
-						}
+				for( j = 0; j < 256 ; j++) {
+					Set<AutomataState> dst = value.get(String.valueOf((char)j));
+					if(dst == null || dst.size() != 1) {
+						text.append("-1");
+					} else {
+						AutomataState st = dst.iterator().next();
+						text.append(st.getID());
+					}
+					if(j + 1 != 256) {
+						text.append(", ");
 					}
 				}
 			}
 			
-			text.append("}\n");
-			text.append("edges[");
-			text.append(var);
-			text.append("] = map");
+			text.append("}");
 			
 			var++;
 		}
+
+
+		text.append("\t\t};");
 				
 		text.append("\n\tif(validate(argv[1], edges) == 1)");
 		text.append("\t\tprintf(\"%s\", \"DFA match\n\");\n");
